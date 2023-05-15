5849d21c375938491a84df9534d036dce1ebfd9f
josef santos <josefsilva02@gmail.com>
2023-05-15 13:17:29 -0300

    good solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,48 +6,53 @@ import java.util.Date;
 import java.util.Scanner;
 
 import model.entities.Reservation;
+import model.exception.DomainException;
 
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args)  {
 
-		Scanner sc = new Scanner(System.in);
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
-			System.out.print("Room number: ");
-			int number = sc.nextInt();
-			System.out.print("Check-in date (dd/MM/yyyy): ");
-			Date checkIn = sdf.parse(sc.next());
-			System.out.print("Check-out date (dd/MM/yyyy): ");
-			Date checkOut = sdf.parse(sc.next());
+	Scanner sc = new Scanner(System.in);
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	try {
+		System.out.print("Room number: ");
+		int number = sc.nextInt();
+		System.out.print("Check-in date (dd/MM/yyyy): ");
+		Date checkIn = sdf.parse(sc.next());
+		System.out.print("Check-out date (dd/MM/yyyy): ");
+		Date checkOut = sdf.parse(sc.next());
 			
-			if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
-			}
-			else {
-				Reservation reservation = new Reservation(number, checkIn, checkOut);
-				System.out.println("Reservation: " + reservation);
-				
-				System.out.println();
-				
-				System.out.println();
-				System.out.println("Enter data to update the reservation:");
-				System.out.print("Check-in date (dd/MM/yyyy): ");
-				checkIn = sdf.parse(sc.next());
-				System.out.print("Check-out date (dd/MM/yyyy): ");
-				checkOut = sdf.parse(sc.next());
-				
-			    String error = reservation.updateDates(checkIn, checkOut);
-			    
-			    if(error != null) {
-					System.out.println("Erro Reservation " + error);
-				}
-				
-				else {
-					System.out.println("Resrvation " + reservation);
-				}
-				
-				
-			sc.close();
-			}}}
+		Reservation reservation = new Reservation(number, checkIn, checkOut);
+		System.out.println("Reservation: " + reservation);
+		
+		System.out.println();
+		
+		System.out.println();	
+		System.out.println("Enter data to update the reservation:");
+		System.out.print("Check-in date (dd/MM/yyyy): ");
+		checkIn = sdf.parse(sc.next());
+		System.out.print("Check-out date (dd/MM/yyyy): ");
+		checkOut = sdf.parse(sc.next());
+		
+	    reservation.updateDates(checkIn, checkOut);
+	    System.out.println("Resrvation " + reservation);
+
+	}
+	catch(ParseException e) {
+		System.out.println("Inavalid date format");
+	}
+	catch (DomainException e) {
+		System.out.println("Error reservation: " + e.getMessage());
+	}
+	catch (RuntimeException e) {
+		System.out.println("Error ensgem inesperada !! ");
+		
+	
+	}
+	
+	System.out.println("Vasco");
+	sc.close();
+	}
+	
+	}
